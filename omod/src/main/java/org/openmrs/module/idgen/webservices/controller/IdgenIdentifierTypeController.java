f8d2f929f88a3f8e40dfe8dabaa5ad455624c4ff
mks-d <dimitri@mekomsolutions.com>
2017-08-11 15:03:14 +0200

    BAH-290: UTF-8 encoding of JSON response of resource /idgen/identifiertype.
      modified:   omod/src/main/java/org/openmrs/module/idgen/webservices/controller/IdgenIdentifierTypeController.java
    The new context-sensitive test for the JSON resource.
      new file:   omod/src/test/java/org/openmrs/module/idgen/webservices/controller/IdgenIdentifierTypeResourceTest.java
    Dependency on openmrs-test for context sensitive tests.
      modified:   omod/pom.xml
      modified:   pom.xml
    Creating a general-purpose constants files and using it.
      new file:   api/src/main/java/org/openmrs/module/idgen/webservices/IdgenWsConstants.java
      modified:   api/src/main/java/org/openmrs/module/idgen/webservices/Activator.java
      modified:   omod/src/main/java/org/openmrs/module/idgen/webservices/services/IdentifierTypeServiceWrapperImpl.java
      modified:   omod/src/test/java/org/openmrs/module/idgen/webservices/services/IdentifierTypeServiceWrapperImplTest.java
      modified:   omod/src/main/java/org/openmrs/module/idgen/webservices/controller/IdgenIdentifierTypeController.java
    Cleaning up imports.
      modified:   omod/src/main/java/org/openmrs/module/idgen/serialization/ObjectMapperRepository.java

--- omod/src/main/java/org/openmrs/module/idgen/webservices/controller/IdgenIdentifierTypeController.java
+++ omod/src/main/java/org/openmrs/module/idgen/webservices/controller/IdgenIdentifierTypeController.java
@@ -1,25 +1,31 @@
 package org.openmrs.module.idgen.webservices.controller;
 
-import org.openmrs.api.APIAuthenticationException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 import org.openmrs.api.context.Context;
 import org.openmrs.module.idgen.contract.IdentifierType;
 import org.openmrs.module.idgen.serialization.ObjectMapperRepository;
+import org.openmrs.module.idgen.webservices.IdgenWsConstants;
 import org.openmrs.module.idgen.webservices.services.IdentifierTypeServiceWrapper;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.IOException;
-import java.util.List;
-
 @Controller
-@RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/idgen/identifiertype")
+@RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/" + IdgenWsConstants.PATH_IDGEN_IDTYPE)
 public class IdgenIdentifierTypeController {
+	
+	public final static String encoding = StandardCharsets.UTF_8.toString();
+	public final static String contentType = "application/json;charset=" + encoding;
 
     @Autowired
     IdentifierTypeServiceWrapper identifierTypeServiceWrapper;
@@ -33,6 +39,8 @@ public class IdgenIdentifierTypeController {
 
         final List<IdentifierType> allIdentifierType = identifierTypeServiceWrapper.getPrimaryAndExtraIdentifierTypes();
         ObjectMapperRepository objectMapperRepository = new ObjectMapperRepository();
-        return new ResponseEntity<String>(objectMapperRepository.writeValueAsString(allIdentifierType), HttpStatus.OK);
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+        headers.set("Content-Type", contentType);
+        return new ResponseEntity<String>(objectMapperRepository.writeValueAsString(allIdentifierType), headers, HttpStatus.OK);
     }
 }
