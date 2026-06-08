ce705cdfec90d07b90d420ec9f3741d59b5122c8
ravinder kabli <ravinder.kabli@thoughtworks.com>
2026-06-08 23:01:58 +0530

    BAH-4460|Updated shebang line sequence

--- scripts/vagrant/vagrant_functions.sh
+++ scripts/vagrant/vagrant_functions.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at https://www.bahmni.org/license/mplv2hd.
@@ -5,7 +6,6 @@
 # Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
 # graphic logo is a trademark of OpenMRS Inc.
 
-#!/bin/bash 
 
 #####################################################################################################
 # This script can be used to call functions which will execute a command in your vagrant box. 
